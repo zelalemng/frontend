e01cbc357548ec844c5563aa2b2d993160159dbb
zelalemng <122828485+zelalemng@users.noreply.github.com>
2024-08-13 15:29:30 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="*" element={<Navigate to="/login" />} />
         </Routes>
         
       )}
