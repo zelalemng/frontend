541b0d7c356ecd5e64168cf3a117825074559779
zelalemng <122828485+zelalemng@users.noreply.github.com>
2024-10-24 00:19:53 +0300

    Update authStore.js

--- src/store/authStore.js
+++ src/store/authStore.js
@@ -62,10 +62,10 @@ export const useAuthStore = create((set) => ({
 		}
 	},
 	checkAuth: async () => {
-		set({ isCheckingAuth: true, error: null });
+		set({ isCheckingAuth: false, error: null });
 		try {
 			const response = await axios.get(`${process.env.REACT_APP_API_URL}/check-auth`);
-			set({ user: response.data.user, isAuthenticated: true, isCheckingAuth: false });
+			set({ user: response.data.user, isAuthenticated: false, isCheckingAuth: false });
 		} catch (error) {
 			set({ error: null, isCheckingAuth: false, isAuthenticated: false });
 		}
