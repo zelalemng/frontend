59632d6bfc9773e2b4af6a5f0ccd83dcd3d9e42c
zelalemng <122828485+zelalemng@users.noreply.github.com>
2024-10-24 12:46:59 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/**
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
@@ -25,3 +26,17 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+**/
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.js";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</React.StrictMode>
+);
