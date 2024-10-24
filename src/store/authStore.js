a562319e942a78a289d2755eaa2d09afdeb5357d
zelalemng <122828485+zelalemng@users.noreply.github.com>
2024-10-24 21:58:53 +0300

    Update authStore.js

--- src/store/authStore.js
+++ src/store/authStore.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import axios from "axios";
 
-const API_URL = "https://agentpro-api.onrender.com";
+//const API_URL = "https://agentpro-api.onrender.com";
 
 axios.defaults.withCredentials = true;
 
