c1b2c74cf5c148f5059e60126441366dee54b163
zelalemng <122828485+zelalemng@users.noreply.github.com>
2024-10-24 17:04:32 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -28,9 +28,11 @@ root.render(
 reportWebVitals();
 **/
 import React from "react";
+import reportWebVitals from "./reportWebVitals";
 import ReactDOM from "react-dom/client";
 import App from "./App.js";
 import "./index.css";
+if (process.env.NODE_ENV === 'production') disableReactDevTools()
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -40,3 +42,5 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 		</BrowserRouter>
 	</React.StrictMode>
 );
+reportWebVitals();
+
